20dd046f59faa93ac7ba4c9cd48cca3dbe3e0061
zerasul <zerasul@gmail.com>
2018-03-18 20:01:00 +0000

    Adding Docstring #16.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ blogrenderer = BlogRenderer(settings.postDir)
 
 @app.route('/')
 def index():
+    """
+    Render the Index page
+    :return: rendered Index Page
+    """
     entry = blogrenderer.renderfile("index")
     template = entry.template
     if template is None:
@@ -18,6 +22,10 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def searchpages():
+    """
+    Render the search page. Must Be on Method POST
+    :return: rendered search Page
+    """
     postlist = blogrenderer.list_posts(search=request.form['search'])
     content = blogrenderer.generatetagpage(postlist)
     return render_template(settings.defaultLayout, tittle=settings.tittle, content=content)
@@ -25,6 +33,11 @@ def searchpages():
 
 @app.route('/tag/<tag>')
 def gettag(tag):
+    """
+    Render the Tags Page.
+    :param tag: Tag for search
+    :return: Rendered tags search.
+    """
     postlist = blogrenderer.list_posts([tag])
     content = blogrenderer.generatetagpage(postlist)
     return render_template(settings.defaultLayout, tittle=settings.tittle, content=content)
@@ -32,6 +45,11 @@ def gettag(tag):
 
 @app.route('/<filename>')
 def getpage(filename):
+    """
+    Render a blog post
+    :param filename: Name of the Blog Post.
+    :return: rendered Blog post or 404 page.
+    """
     try:
         entry = blogrenderer.renderfile(filename)
     except PageNotExistError:
--- settings.py
+++ settings.py
@@ -1,5 +1,20 @@
 templateDir = "templates"
+"""
+Name of the Template Folder.
+"""
 postDir = "posts"
+"""
+Name of the post Folder
+"""
 defaultLayout = "template.html"
+"""
+default name of the template file.
+"""
 staticDir = "static"
+"""
+Default name of the static Folder
+"""
 tittle = "Blask | A Simple Blog Engine Based on Flask"
+"""
+Title of the blog
+"""
