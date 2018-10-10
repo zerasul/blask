caac0c3b96c7fe70904bd4f0407f52cf68fb70dd
Juanjo Salvador <juanjosalvador@netc.eu>
2018-10-10 22:45:21 +0200

    removed unnecesary script

--- createdist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Blask Upload Script; create a new module and upload to pypi.
-# this script requires python 3.4+ and the next modules:
-# - m2r
-# - twine
-# this script require a valid user and password for upload to Pypi.
-rm -R dist
-python setup.py sdist bdist_wheel
-twine upload dist/Blask-*
