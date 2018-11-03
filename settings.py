2f412cd0dbbe3dd6c49b972e59e65dbdf8a526bf
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-11-03 12:58:17 +0100

    using pathlib instead of os.path in settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,23 @@
 import os
+from pathlib import Path
 
-BASE_DIR = os.getcwd()
+#BASE_DIR = os.getcwd()
+BASE_DIR = Path(__file__).resolve().parents[0]
 
 # Templates directory
-templateDir = os.path.join(BASE_DIR, 'templates')
+#templateDir = os.path.join(BASE_DIR, 'templates')
+templateDir = BASE_DIR / 'templates'
 
 # Posts directory
-postDir = os.path.join(BASE_DIR, 'posts')
+#postDir = os.path.join(BASE_DIR, 'posts')
+postDir = BASE_DIR / 'posts'
 
 # Default layout template
 defaultLayout = "template.html"
 
 # Static files directory
-staticDir = os.path.join(BASE_DIR, 'static')
+#staticDir = os.path.join(BASE_DIR, 'static')
+staticDir = BASE_DIR / 'static'
 
 # Website title
 title = 'Blask | A Simple Blog Engine Based on Flask'
