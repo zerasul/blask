70a0df9440254aa8ba0f5420be9a7f9dfca983c1
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-11-03 13:00:05 +0100

    remove commented os.path commands

--- settings.py
+++ settings.py
@@ -1,22 +1,17 @@
-import os
 from pathlib import Path
 
-#BASE_DIR = os.getcwd()
 BASE_DIR = Path(__file__).resolve().parents[0]
 
 # Templates directory
-#templateDir = os.path.join(BASE_DIR, 'templates')
 templateDir = BASE_DIR / 'templates'
 
 # Posts directory
-#postDir = os.path.join(BASE_DIR, 'posts')
 postDir = BASE_DIR / 'posts'
 
 # Default layout template
 defaultLayout = "template.html"
 
 # Static files directory
-#staticDir = os.path.join(BASE_DIR, 'static')
 staticDir = BASE_DIR / 'static'
 
 # Website title
