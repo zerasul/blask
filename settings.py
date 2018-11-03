8c269a477740f9e7fae93fbd40ec4b71b6eea180
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-11-03 18:46:42 +0100

    path to string with pathlib

--- settings.py
+++ settings.py
@@ -3,16 +3,16 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[0]
 
 # Templates directory
-templateDir = BASE_DIR / 'templates'
+templateDir = str(BASE_DIR / 'templates')
 
 # Posts directory
-postDir = BASE_DIR / 'posts'
+postDir = str(BASE_DIR / 'posts')
 
 # Default layout template
 defaultLayout = "template.html"
 
 # Static files directory
-staticDir = BASE_DIR / 'static'
+staticDir = str(BASE_DIR / 'static')
 
 # Website title
 title = 'Blask | A Simple Blog Engine Based on Flask'
