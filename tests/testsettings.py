176b6419a1f8ed49bf32622d23140b321f32de7d
Cristóbal Contreras Rubio <crisconru@gmail.com>
2018-11-03 14:21:06 +0100

    pathlib instead of os.path

--- tests/testsettings.py
+++ tests/testsettings.py
@@ -1,18 +1,18 @@
-import os
+from pathlib import Path
 
-BASE_DIR = os.getcwd()
+BASE_DIR = Path(__file__).resolve().parents[0]
 
 # Templates directory
-templateDir = os.path.join(BASE_DIR, 'templates')
+templateDir = BASE_DIR / 'templates'
 
 # Posts directory
-postDir = os.path.join(BASE_DIR, 'posts2')
+postDir = BASE_DIR / 'posts2'
 
 # Default layout template
 defaultLayout = "template.html"
 
 # Static files directory
-staticDir = os.path.join(BASE_DIR, 'static')
+staticDir = BASE_DIR / 'static'
 
 # Website title
 title = 'The mantis revenge!'
