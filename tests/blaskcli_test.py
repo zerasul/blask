c4218f5d35514d96cc1e5b435170a1c20dac2d74
zerasul <zerasul@gmail.com>
2020-03-13 18:44:58 +0100

    adding mocks to improve coverage

--- tests/blaskcli_test.py
+++ tests/blaskcli_test.py
@@ -13,4 +13,8 @@ class TestCLI:
         mocker.patch('shutil.copy')
         run = self.runner.invoke(blaskcli.blaskcli, ['init'])
         assert "Initializing new Blask Project" in run.output
-
+    
+    def test_run(self, mocker):
+        mocker.patch('Blask.blaskcli.blask.run')
+        run = self.runner.invoke(blaskcli.blaskcli, ['run'])
+        assert "Blask (C)" in run.output
